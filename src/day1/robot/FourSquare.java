575069c7a8ae6b06588a20f1022099ac9077bb60
390852 <league@Mac-mini-2.local>
2016-11-22 16:02:09 -0800

    I did so swag things today

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,25 +6,41 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot bill = new Robot();
 	
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+		bill.setSpeed(10);
 
 		// 5. Set the pen width to 5
-
+		bill.setPenWidth(5);
+ bill.penDown();
 		// 6. Do steps #7 to #8 four times...
+		for (int i = 0; i < 4; i++) {
+			
+		
+		
 
 			// 7. Set the pen color to random
+		bill.setRandomPenColor();
 	
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
+		bill.turn(90);
+		}
 
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
-	void drawSquare() {
+	void drawSquare() { 
+		for (int i = 0; i < 4; i++) { 
+	bill.turn(360/4);
+	bill.move(250);
+			
+		}
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 
 		
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,22 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot steve = new Robot();
+		steve.move(150);
+		steve.penDown();
+		steve.setPenColor(300,17,105);
+		for (int i = 0; i < 5; i++) {
+		steve.move(90);
+		steve.turn(360/5);
+			
+		}
+	}
+}
+		
+		
+        		
 
 	
 		
 		
-	}
-}
+
